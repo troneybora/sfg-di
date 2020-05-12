8c293af8ba5d1b13cda6f165bf88eebf644e2796
Troney <troneybora@hotmail.com>
2020-05-12 15:10:01 +0530

    Dependency Injection with Qualifiers - Primary Bean Service(Addition of @Primary)

--- src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -1,7 +1,8 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
-
+@Primary
 @Service
 public class PrimaryGreetingService implements GreetingService {
     @Override
